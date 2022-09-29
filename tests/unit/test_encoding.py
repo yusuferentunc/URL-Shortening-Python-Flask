fdd657ef3874d0635ecfb6d737cbba82b0ca78b9
Yusuf Eren Tunç <erentunc97@gmail.com>
2022-09-29 15:18:11 +0300

    Create unit test for url encode function

---
+++ tests/unit/test_encoding.py
@@ -0,0 +1,19 @@
+from app import encoding
+
+
+def test_encode_with_id():
+    test_id = 52223117
+    code = encoding(test_id)
+    assert code == 'http://short.est/v3bm5'
+
+
+def test_encode_with_zero_id():
+    test_id = 0
+    code = encoding(test_id)
+    assert code is None
+
+
+def test_encode_with_wrong_input():
+    test_id = 'irrelavant input'
+    code = encoding(test_id)
+    assert code is None
