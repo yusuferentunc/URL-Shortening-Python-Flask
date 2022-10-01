e32545d0b4e497f68cfe07aad2fa0234d6435ed5
eren <erentunc97@gmail.com>
2022-10-01 20:01:57 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='url-shortening',
+    version='1.0.0',
+    packages=['tests', 'tests.unit', 'tests.functional'],
+    author='Yusuf Eren Tunc',
+    description='URL Shortening service written in Flask without database usage.'
+)
