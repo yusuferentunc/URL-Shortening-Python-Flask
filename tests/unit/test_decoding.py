dbf22a3eacd4e4fabae469e175b90a70d749650a
eren <erentunc97@gmail.com>
2022-10-01 19:30:03 +0300

    Add docstrings to unit tests for readability

--- tests/unit/test_decoding.py
+++ tests/unit/test_decoding.py
@@ -2,18 +2,21 @@ from main import decoding
 
 
 def test_decode_with_url():
+    """Test decode function in normal behaviour"""
     code = 'v3bm5'
     code_id = decoding(code)
     assert code_id == 52223117
 
 
 def test_decode_with_empty_code():
+    """Test decode function with empty string/code"""
     code = ''
     code_id = decoding(code)
     assert code_id == 0
 
 
 def test_decode_with_wrong_input():
+    """Test decode function with input that is in wrong format"""
     code = 1234
     code_id = decoding(code)
     assert code_id is None
--- tests/unit/test_encoding.py
+++ tests/unit/test_encoding.py
@@ -2,18 +2,21 @@ from main import encoding
 
 
 def test_encode_with_id():
+    """Test encode function in normal behaviour"""
     test_id = 52223117
     code = encoding(test_id)
     assert code == 'http://short.est/v3bm5'
 
 
 def test_encode_with_zero_id():
+    """Test encode function with zero id"""
     test_id = 0
     code = encoding(test_id)
     assert code is None
 
 
 def test_encode_with_wrong_input():
+    """Test encode function with input that is in wrong format"""
     test_id = 'irrelavant input'
     code = encoding(test_id)
     assert code is None
